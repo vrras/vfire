e8062c3c6cfd315e5f0a62ea4561da6019998d4b
Firas Luthfi <vrrases@gmail.com>
2020-12-16 22:32:35 +0700

    perbaikan

--- firebase.ts
+++ firebase.ts
@@ -5,7 +5,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-const init = (config): { firebase; FireSQL } => {
+const init = (config): { firebase, FireSQL } => {
   if (!firebase.apps.length) {
     firebase.initializeApp(config);
   }
